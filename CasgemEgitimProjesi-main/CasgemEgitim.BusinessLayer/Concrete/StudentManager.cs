aac8fe3e15675beb553505efd01a6e81b4a55a96
nurgulyucel <nnurgulyucell@gmail.com>
2023-07-27 21:43:29 +0300

    Teacher View-Controller

---
+++ CasgemEgitimProjesi-main/CasgemEgitim.BusinessLayer/Concrete/StudentManager.cs
@@ -0,0 +1,46 @@
+﻿using CasgemEgitim.BusinessLayer.Abstract;
+using CasgemEgitim.DataAccessLayer.Abstract;
+using CasgemEgitim.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemEgitim.BusinessLayer.Concrete
+{
+    public class StudentManager : IStudentService
+    {
+        private readonly IStudentDal _studentDal;
+
+        public StudentManager(IStudentDal studentDal)
+        {
+            _studentDal = studentDal;
+        }
+
+        public void TDelete(Student t)
+        {
+            _studentDal.Delete(t);
+        }
+
+        public Student TGetById(int id)
+        {
+           return _studentDal.GetById(id);
+        }
+
+        public List<Student> TGetList()
+        {
+            return _studentDal.GetList();
+        }
+
+        public void TInsert(Student t)
+        {
+            _studentDal.Insert(t);
+        }
+
+        public void TUpdate(Student t)
+        {
+            _studentDal.Update(t);
+        }
+    }
+}
---
+++ CasgemEgitimProjesi-main/CasgemEgitim.DataAccessLayer/Migrations/20230727170523_mig_first.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CasgemEgitim.DataAccessLayer.Migrations
+{
+    public partial class mig_first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactAdress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactMaps = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CourseImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CourseDes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CoursePrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CourseStudent = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.CourseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    TeachertId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeacherName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeacherSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeacherUsername = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeacherPassword = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeacherImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.TeachertId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
