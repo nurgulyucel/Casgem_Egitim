9eacb40a9b71df58c64392bb018b15833c25c7e8
nurgulyucel <nnurgulyucell@gmail.com>
2023-08-14 22:38:00 +0300

    Register

---
+++ CasgemEgitimProjesi-main (2)/CasgemEgitimProjesi-main/CasgemEgitim.PresentationLayer/Controllers/StudentController.cs
@@ -0,0 +1,102 @@
+﻿using CasgemEgitim.BusinessLayer.Abstract;
+using CasgemEgitim.EntityLayer.Concrete;
+using CasgemEgitim.ModelViewLayer.ModelView.Comment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemEgitim.PresentationLayer.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentService _studentService;
+        private readonly ICourseService _courseService;
+        private readonly ICourseDetailService _courseDetailService;
+        private readonly ICommentService _commentService;
+
+        public StudentController(IStudentService studentService, ICourseService courseService, ICourseDetailService courseDetailService, ICommentService commentService)
+        {
+            _studentService = studentService;
+            _courseService = courseService;
+            _courseDetailService = courseDetailService;
+            _commentService = commentService;
+        }
+
+        public IActionResult Index()
+        {
+            var userName = HttpContext.Session.GetString("username");
+            var s = _studentService.TGetStudentByUsername(userName);
+            ViewBag.st = s;
+            var values = _studentService.TGetList();
+            return View(values);
+        }
+
+
+        public IActionResult ListUserCourse()
+        {
+            var values = _courseService.TGetCoursesWithUserStudent(2);
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            _studentService.TInsert(student);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteStudent(int id)
+        {
+            var values = _studentService.TGetById(id);
+            _studentService.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateStudent(int id)
+        {
+            var values = _studentService.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateStudent(Student student)
+        {
+            var values = _studentService.TGetById(student.StudentId);
+            values.StudentName = student.StudentName;
+            values.StudentSurname = student.StudentSurname;
+            values.Username = student.Username;
+            values.ImageUrl = student.ImageUrl;
+            values.Password = student.Password;
+            _studentService.TUpdate(values);
+            return RedirectToAction("Index");
+        }
+
+        //kurs Detay işlemleri
+
+        public IActionResult ListCourseDetail(int id)
+        {
+            ViewBag.CourseName = _courseDetailService.TGetCourseByIdWithCourseName(id);
+            var values = _courseDetailService.TGetCoursesWithById(id);
+            return View(values);
+        }
+
+
+        [HttpPost]
+        public IActionResult Comment(CreateCommentMV p)
+        {
+            Comment model = new Comment()
+            {
+               CourseId = 2,
+               StudentId=2,
+               CommentMessage = p.CommentMessage
+
+            };
+            _commentService.TInsert(model);
+            return RedirectToAction("ListUserCourse");
+        }
+    }
+}
