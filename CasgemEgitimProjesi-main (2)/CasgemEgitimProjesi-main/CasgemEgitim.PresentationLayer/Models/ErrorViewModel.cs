db3bcafa09396c047ccf3931f05645ca44b471bb
nurgulyucel <nnurgulyucell@gmail.com>
2023-08-17 04:34:21 +0300

    Authentication

--- CasgemEgitimProjesi-main (2)/CasgemEgitimProjesi-main/CasgemEgitim.PresentationLayer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CasgemEgitim.PresentationLayer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
